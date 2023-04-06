d21a242ed077e462f1776705ae7475e0f0c3adc1
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-04-06 07:47:13 +0100

    Added if and else statements

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-let firstCard = 2;
-let secondCard = 7;
-
+let firstCard = 10;
+let secondCard = 11;
 let sum = firstCard + secondCard
 
-console.log(sum)
+if (sum < 21) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+    console.log("You're out of the game! 😭")
+}
