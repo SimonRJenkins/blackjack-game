e04cc1f8aa30d6e3df1b7c66fb8b3f20baacd817
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-04-07 07:36:38 +0100

    Added styling for the webpage and isAlive and hasBlackJack booleans

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 let firstCard = 10;
-let secondCard = 11;
+let secondCard = 10;
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-} else {
-    console.log("You're out of the game! 😭")
+if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂"
 }
+else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
+}
+else {
+    message = "You're out of the game! 😭"
+    isAlive = false
+}
+console.log(isAlive)
+console.log(message)
