3218d765b4281fe9a4f1caf4d2986929e1cb1c29
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-04-11 08:19:16 +0100

    Added an array for the cards and renamed function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let firstCard = 10;
-let secondCard = 11;
+let firstCard = 10
+let secondCard = 6
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -10,8 +11,12 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
+    renderGame()
+}
+
+function renderGame() {
     sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard
+    cardsEl.textContent = "Cards: " + cards[0] + ", " + cards[1]
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     }
@@ -28,4 +33,9 @@ function startGame() {
 
 function newCard() {
     console.log("Drawing a new card from the deck!")
+    let card = 5
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    renderGame()
 }
